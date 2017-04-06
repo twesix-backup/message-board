30206a7900bef2aa0a68ff3557bcbee5c8337ff4
孟政元 <278227739@qq.com>
2017-04-06 21:55:50 +0800

    一些基本的文件框架

---
+++ front-end/src/components/add.vue
@@ -0,0 +1,28 @@
+<template>
+    <form class="form">
+        <div class="form-group">
+            <label for="message">留言内容</label>
+            <textarea v-model="message" id="message" class="form-control"></textarea>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-default">提交</button>
+    </form>
+</template>
+<script>
+    module.exports =
+        {
+            data: function()
+            {
+                return {
+                    message: '输入你的留言...'
+                };
+            },
+            methods:
+                {
+                    submit: function()
+                    {
+
+                    }
+                }
+        }
+</script>
---
+++ front-end/src/components/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul>
+            <router-link to="/register" tag="li">注册</router-link>
+            <router-link to="/login" tag="li">登录</router-link>
+            <router-link to="/add" tag="li">发表留言</router-link>
+            <router-link to="/list" tag="li">查看留言</router-link>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    module.exports =
+        {
+            data: function()
+            {
+                return {
+
+                };
+            }
+        }
+</script>
